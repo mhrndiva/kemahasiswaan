f626edb3cd815b5f6a542e5fe03dbeb81fa318de
mhrndiva <ariadiva00@gmail.com>
2024-12-09 17:56:28 +0700

    bosok

--- module/tugas.go
+++ module/tugas.go
@@ -55,7 +55,7 @@ func GetMahasiswaFromID(_id primitive.ObjectID, db *mongo.Database, col string) 
 	return mahasiswa, nil
 }
 
-func GetMahasiswaFromNPM(npm int) (staf model.Dosen) {
+func GetMahasiswaFromNPM(npm int) (staf model.Mahasiswa) {
     mahasiswa := MongoConnect("data_mahasiswa").Collection("mahasiswa")
     filter := bson.M{"npm": npm}
     err := mahasiswa.FindOne(context.TODO(), filter).Decode(&staf)
@@ -80,7 +80,7 @@ func GetAllMahasiswa() (data [] model.Mahasiswa) {
 	return
 }
 
-func GetAllDosen() (data [] model.Mahasiswa) {
+func GetAllDosen() (data [] model.Dosen) {
 	dosen := MongoConnect("data_mahasiswa").Collection("dosen")
 	filter := bson.M{}
 	cursor, err := dosen.Find(context.TODO(), filter)
